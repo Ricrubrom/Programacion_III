ad2d4959f03949eea46a8d88338ce7f039f72ac7
Mateo <materomero04@gmail.com>
2023-04-11 18:37:46 -0300

    Practica 5 Parte B Ejercicio 3 completado

--- Practica5/ParteB/Ejercicio3/TRIE.java
+++ Practica5/ParteB/Ejercicio3/TRIE.java
@@ -8,16 +8,13 @@ public class TRIE {
   
   public TRIE() {
     a = new ArbolGeneral<Character>(null);
-
   }
 
   public void agregarPalabra(String palabra) {
     ArbolGeneral<Character> arbol = this.a;
     for (int i = 0; i < palabra.length(); i++) {
       char car = palabra.charAt(i);
-      System.out.println(car);
       if (arbol.esVacio() || arbol.esHoja()) {
-        System.out.println(car+ " es hoja");
         ArbolGeneral<Character> aux = new ArbolGeneral<Character>(car);
         arbol.agregarHijo(aux);
         arbol = aux;
@@ -28,17 +25,14 @@ public class TRIE {
       int j = 0;
       while (!hijos.fin()) {
         ArbolGeneral<Character> act = hijos.proximo();
-        System.out.println();
         if (act.getDato() >= car) {
           break;
         }
         j++;
       }
-      if (hijos.elemento(j)!=null && hijos.elemento(j).getDato() == car) {
-        System.out.println("me muevo a " + hijos.elemento(j).getDato());
+      if (hijos.elemento(j) != null && hijos.elemento(j).getDato() == car) {
         arbol = hijos.elemento(j);
       } else {
-        System.out.println("agrego " + car);
         ArbolGeneral<Character> aux = new ArbolGeneral<Character>(car);
         hijos.agregarEn(aux, j);
         arbol = hijos.elemento(j);
@@ -46,6 +40,103 @@ public class TRIE {
     }
     
   }
+  
+
+  /*public ListaGenerica<StringBuilder> palabrasQueEmpiezanCon(String prefijo) {
+    ListaGenerica<StringBuilder> lista = new ListaGenericaEnlazada<StringBuilder>();
+    ArbolGeneral<Character> arbol = this.a;
+    StringBuilder aux = new StringBuilder();
+    aux.append(prefijo);
+    for (int i = 0; i < prefijo.length(); i++) {
+      char car = prefijo.charAt(i);
+      ListaGenerica<ArbolGeneral<Character>> hijos = arbol.getHijos();
+      hijos.comenzar();
+      int j = 0;
+      while (!hijos.fin()) {
+        ArbolGeneral<Character> act = hijos.proximo();
+        if (act.getDato() >= car) {
+          break;
+        }
+        j++;
+      }
+      if (hijos.elemento(j) != null && hijos.elemento(j).getDato() == car) {
+        arbol = hijos.elemento(j);
+      } else {
+        return null;
+      }
+    }
+  
+    Pila<ArbolGeneral<Character>> pila = new Pila<>();
+    ListaGenerica<ArbolGeneral<Character>> h = arbol.getHijos();
+    h.comenzar();
+    while (!h.fin()) {
+      pila.apilar(h.proximo());
+    }
+    int i = 0;
+    while (!pila.esVacia()) {
+      ArbolGeneral<Character> act = pila.desapilar();
+      while (!act.esHoja()) {
+        ListaGenerica<ArbolGeneral<Character>> hijos = act.getHijos();
+        hijos.comenzar();
+        if (hijos.tamanio() > 1) {
+          hijos.comenzar();
+          while (!hijos.fin())
+            pila.apilar(hijos.proximo());
+        }
+        if (lista.elemento(i) == null)
+          lista.agregarFinal(aux.append(act.getDato()));
+        else
+          lista.elemento(i).append(act.getDato());
+        act = hijos.proximo();
+      }
+      lista.elemento(i).append(act.getDato());
+      i++;
+    }
+  
+    return lista;
+  }*/
+
+  public ListaGenerica<StringBuilder> palabrasQueEmpiezanCon(String prefijo) {
+    ListaGenerica<StringBuilder> lista = new ListaGenericaEnlazada<StringBuilder>();
+    ArbolGeneral<Character> arbol = this.a;
+    for (int i = 0; i < prefijo.length(); i++) {
+        char car = prefijo.charAt(i);
+        ListaGenerica<ArbolGeneral<Character>> hijos = arbol.getHijos();
+        hijos.comenzar();
+        int j = 0;
+        while (!hijos.fin()) {
+            ArbolGeneral<Character> act = hijos.proximo();
+            if (act.getDato() >= car) {
+                break;
+            }
+            j++;
+        }
+        if (hijos.elemento(j) != null && hijos.elemento(j).getDato() == car) {
+            arbol = hijos.elemento(j);
+        } else {
+            return null;
+        }
+    }
+
+    StringBuilder palabra = new StringBuilder(prefijo);
+    recorrerArbol(arbol, palabra, lista);
+    return lista;
+}
+
+private void recorrerArbol(ArbolGeneral<Character> nodo, StringBuilder palabra, ListaGenerica<StringBuilder> lista) {
+    if (nodo.esHoja()) {
+        lista.agregarFinal(new StringBuilder(palabra));
+        return;
+    }
+    ListaGenerica<ArbolGeneral<Character>> hijos = nodo.getHijos();
+    hijos.comenzar();
+    while (!hijos.fin()) {
+        ArbolGeneral<Character> hijo = hijos.proximo();
+        palabra.append(hijo.getDato());
+        recorrerArbol(hijo, palabra, lista);
+        palabra.deleteCharAt(palabra.length() - 1);
+    }
+}
 
   public void ImprimirPorNiveles() {
     a.ImprimirPorNiveles();
--- Practica5/ParteB/Ejercicio3/TestTRIE.java
+++ Practica5/ParteB/Ejercicio3/TestTRIE.java
@@ -3,12 +3,15 @@ package Practica5.ParteB.Ejercicio3;
 public class TestTRIE {
   public static void main(String[] args) {
     TRIE trie = new TRIE();
+    trie.agregarPalabra("arbol");
+    trie.agregarPalabra("araña");
+    trie.agregarPalabra("arañita");
+    trie.agregarPalabra("arar");
     trie.agregarPalabra("hola");
     trie.agregarPalabra("hoja");
     trie.agregarPalabra("homo");
-    trie.agregarPalabra("arbol");
-    trie.agregarPalabra("araña");
-    System.out.println("\n\n");
     trie.ImprimirPorNiveles();
+    System.out.println("\n");
+    System.out.println(trie.palabrasQueEmpiezanCon("ara"));
   }
 }
